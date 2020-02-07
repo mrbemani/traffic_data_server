37ef71a3c0c69c4bd59422bb155279ef19bce77a
mrbemani <kawaii_dav@qq.com>
2020-02-07 14:12:13 +0800

    add tsu_generate_thumbnails

--- controllers/tsutils.py
+++ controllers/tsutils.py
@@ -3,6 +3,7 @@
 
 import os
 import shutil
+from PIL import Image
 
 def tsu_rearrange_snapshots():
     startidx = 0
@@ -30,5 +31,35 @@ def tsu_rearrange_snapshots():
         last_id = itm.id
     return dict(outtext=outtext, idx=last_id)
 
+
+def tsu_generate_thumbnails():
+    startidx = 0
+    maxlen = 1000
+    outtext = ""
+    if "idx" in request.get_vars:
+        startidx = int(request.get_vars["idx"])
+    capdir = os.path.join(os.path.abspath('.'), "applications", request.application, "static", "capture", "vehicles")
+    rs = db(db.photo._id>startidx).select(db.photo.ALL, limitby=(0, maxlen))
+    last_id = startidx
+    thumbsize = (60, 60)
+    thumbs_dir = os.path.join(capdir, "thumbs")
+    for ph in rs:
+        fin = os.path.join(capdir, ph.name)
+        ph_name = ph.name.split('.')
+        ph_new_name = ".".join([*ph_name[:-1], "thumb_60x60", ph_name[-1]])
+        fout = os.path.join(thumbs_dir, ph_new_name)
+        if os.path.exists(thumbs_dir):
+            if not os.path.isdir(thumbs_dir):
+                os.unlink(thumbs_dir)
+                os.mkdir(thumbs_dir)
+        else:
+            os.mkdir(thumbs_dir)
+        im = Image.open(fin)
+        im.thumbnail(thumbsize)
+        im.save(fout, "JPEG")
+        outtext += "[{}] {}... \r\n".format(ph.id, fin)
+        last_id = ph.id
+        db.mytable[ph.id] = dict(thumbnail="thumbs/{}".format(ph_new_name))
+    return dict(outtext=outtext, idx=last_id)
 
     
