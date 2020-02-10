94639130a706b94421f7e91cdbe73cd24c39ffb8
mrbemani <smile.ds@gmail.com>
2020-02-11 01:04:57 +0800

    bugfix

--- controllers/sync.py
+++ controllers/sync.py
@@ -111,6 +111,6 @@ def update_equipment_runtime():
     import sys
     try:
         db_ret = db.executesql("UPDATE equipment_runtime SET data_json=%s WHERE gid=%s;", (data_json, gid))
-        return db_ret
+        return data_json
     except:
         return sys.exc_info()[1]
